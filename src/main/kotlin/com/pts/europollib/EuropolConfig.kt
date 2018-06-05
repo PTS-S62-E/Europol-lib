fdede857844596bc3d9d7f9124cffff054d4c009
teunw <teun.willems+github@live.nl>
2018-06-05 12:39:46 +0200

    Compat with V2

--- src/main/kotlin/com/pts/europollib/EuropolConfig.kt
+++ src/main/kotlin/com/pts/europollib/EuropolConfig.kt
@@ -3,5 +3,5 @@ package com.pts.europollib
 import okhttp3.HttpUrl
 
 open class EuropolConfig {
-    fun createUrl() = "http://192.168.24.101:8000/api/vehicles/"
+    fun createUrl() = "http://192.168.24.101:8000/api/v2/vehicles/"
 }
--- src/main/kotlin/com/pts/europollib/EuropolVehicle.kt
+++ src/main/kotlin/com/pts/europollib/EuropolVehicle.kt
@@ -3,7 +3,7 @@ package com.pts.europollib
 import java.io.Serializable
 
 data class EuropolVehicle(
-        val HashedLicensePlate: String = "",
+        val licensePlate: String = "",
         val serialNumber: String = "",
         val originCountry: String = ""
 ) : Serializable
